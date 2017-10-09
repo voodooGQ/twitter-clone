86bafc7d0e84b1a6017a1a07b0d517a6473016f5
Shane Smith <voodoogq@gmail.com>
2017-10-09 13:44:55 -0500

    Sessions controller tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -16,9 +16,20 @@ RSpec.describe SessionsController, type: :controller do
 
   describe "POST #create" do
     context "when properly authenticated" do
-      it "sets the user_id session variable to the user's id" do
+      before do
         post :create, params: { email: @user.email, password: @user.password }
+      end
+
+      it "sets the user_id session variable to the user's id" do
         expect(session[:user_id]).to eq(@user.id)
+      end
+
+      it "redirects the user to the root_url" do
+        expect(response).to redirect_to(root_url)
+      end
+
+      it "sends a 'Logged in!' flash notice" do
+        expect(controller).to set_flash[:notice].to(/Logged in!/)
       end
     end
   end
