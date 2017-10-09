1ae8dfa9b92c28e45ac6f49f1cd24071dfc1498a
Shane Smith <voodoogq@gmail.com>
2017-10-09 13:39:05 -0500

    Session controller tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,6 +1,11 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require "rails_helper"
 
 RSpec.describe SessionsController, type: :controller do
+  before do
+    @user = create(:user)
+  end
 
   describe "GET #new" do
     it "returns http success" do
@@ -9,4 +14,15 @@ RSpec.describe SessionsController, type: :controller do
     end
   end
 
+  describe "POST #create" do
+    context "when properly authenticated" do
+      it "sets the user_id session variable to the user's id" do
+        post :create, params: { email: @user.email, password: @user.password }
+        expect(session[:user_id]).to eq(@user.id)
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+  end
 end
