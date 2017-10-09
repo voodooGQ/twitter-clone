d4e57a2c08eb5ba18685f45155abeba41dca3591
Shane Smith <voodoogq@gmail.com>
2017-10-09 15:42:33 -0500

    Working through user controller specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,18 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require "rails_helper"
 
 RSpec.describe UsersController, type: :controller do
+  let(:subject) { described_class }
+  describe "GET #new" do
+    before { get :new }
 
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+
+    it "assigns a new user object to the @user instance variable" do
+      expect(assigns(:user)).to be_an(User)
+    end
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -39,6 +39,8 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
 
+  config.include AuthHelper, type: :controller
+
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
 
