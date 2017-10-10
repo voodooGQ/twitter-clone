f56b53d9041f7b41a34eaac83e7d2b5e01a317f1
Shane Smith <voodoogq@gmail.com>
2017-10-10 11:24:17 -0500

    Redirect home to feed page if logged in.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 #  frozen_string_literal: true
 
 class StaticPagesController < ApplicationController
-  def home; end
+  def home
+    # Automatically redirect to a users feed page if they're logged in.
+    redirect_to feed_url if current_user
+  end
 end
