ce8159d46e914f960aed6309a4f407168db74a82
Shane Smith <voodoogq@gmail.com>
2017-10-11 19:05:54 -0500

    Started working through chirp display

--- app/controllers/chirps_controller.rb
+++ app/controllers/chirps_controller.rb
@@ -1,22 +1,26 @@
 class ChirpsController < ApplicationController
-  def index
-    authorize
-    @chirps = Chirp.all
-  end
-
-  def new
-    @chirp = Chirp.new
-  end
+  before_action :correct_user, only: :destroy
+  before_action :authorize, only: [:create, :destroy]
 
   def create
-    redirect_to root_url, notice: "You must be logged in." unless current_user
-    @chirp = Chirp.new(chirp_params[:chirp].merge(user: current_user))
-    render @chirp.save ? :index : :new
+    @chirp = current_user.chirps.build(chirp_params)
+    if @chirp.save
+      flash[:success] = "Chirp created!"
+    else
+      flash[:error] = "Uh oh, something went wrong, try again!"
+    end
+
+    redirect_to feed_url
   end
 
   private
 
   def chirp_params
     params.require(:chirp).permit(:message)
+  end
+
+  def correct_user
+    @chirp = current_user.chirps.find_by(id: params[:id])
+    redirect_to root_url if @chirp.nil?
   end
 end
--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -2,5 +2,6 @@ class FeedController < ApplicationController
   def index
     authorize
     @feed = current_user.chirp_feed
+    @chirp  = current_user.chirps.build
   end
 end
