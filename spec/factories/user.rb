604f7897f82d40b26097051383d2d507844bcfee
Shane Smith <voodoogq@gmail.com>
2017-10-15 00:34:53 -0500

    UserHelper tests

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
   factory :user do
     sequence(:username) {|n| "user#{n}" }
     sequence(:email) {|n| "user#{n}@email.com"}
+    image_path { nil }
     password { "abcd1234" }
     password_confirmation { "abcd1234" }
   end
