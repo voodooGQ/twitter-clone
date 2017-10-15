4380617cc8f127bd23d48e04a37443912b3aa91a
Shane Smith <voodoogq@gmail.com>
2017-10-15 01:08:12 -0500

    LikesController tests

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,7 +5,7 @@ class LikesController < ApplicationController
     @chirp = Chirp.find(params[:like][:chirp_id])
     current_user.like!(@chirp)
     respond_to do |format|
-      format.html { redirect_to current_page_url }
+      format.html { redirect_back(fallback_location: feed_url) }
       format.js
     end
   end
@@ -14,7 +14,7 @@ class LikesController < ApplicationController
     @chirp = Like.find(params[:id]).chirp
     current_user.unlike!(@chirp)
     respond_to do |format|
-      format.html { redirect_to current_page_url }
+      format.html { redirect_back(fallback_location: feed_url) }
       format.js
     end
   end
